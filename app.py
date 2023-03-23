0c2a04e111681118fa9326bb672e6f1437c7c194
gitmj <manoj41@gmail.com>
2023-03-23 01:04:57 -0700

    Changed iframe to embed because pdf was not displaying in cloud.

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800"' \
-                  f' type="application/pdf"></iframe>'
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800"' \
+                  f' type="application/pdf"></embed>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
     # components.iframe(f"data:application/pdf;base64,{base64_pdf}", height=800, scrolling=True)
